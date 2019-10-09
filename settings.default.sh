d28d034b9d66f4d022a6b91356173599a88a085c
Lionel Sambuc <lionel.sambuc@epfl.ch>
2019-10-09 11:18:15 +0200

    Enforce max memory usage limit
     * By default this is currently set to 8GB
     * Swap is disabled

--- settings.default.sh
+++ settings.default.sh
@@ -21,4 +21,4 @@
 : ${KG_SPATIAL_SEARCH_SERVICE_NAME:="${COMPOSE_PROJECT_NAME}_spatial-search_1"}
 : ${KG_SPATIAL_SEARCH_LOG_LVL:="warn,actix_web=info,mercator_service=info"}
 : ${KG_SPATIAL_SEARCH_ALLOWED_ORIGINS:="http://localhost:3200"}
-
+: ${KG_SPATIAL_SEARCH_MAX_MEM:=$((8 * 1073741824))} # Max X x 1GB
